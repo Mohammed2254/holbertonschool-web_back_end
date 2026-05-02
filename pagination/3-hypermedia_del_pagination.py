55f21b2179d4cd712e0ab6f5c407fcffb65fe32b
Mohammed2254 <m.a.alabduli@gmail.com>
2026-05-02 23:24:19 +0300

    pycodestyle problems solved.3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -6,7 +6,6 @@ Deletion-resilient hypermedia pagination
 import csv
 from typing import List, Dict
 
-
 class Server:
     """
     Server class to paginate a database of popular baby names.
@@ -35,10 +34,10 @@ class Server:
         Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
-         dataset = self.dataset()
-         truncated_dataset = dataset[:1000]
-         self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
-         return self.__indexed_dataset
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
+        return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
